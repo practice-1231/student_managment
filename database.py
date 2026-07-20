78cdb539612f0fbe7e54b6dfa271dbaaac29f48c
Suleman <salmaan.waheed78910@gmail.com>
2026-07-20 09:11:36 +0500

    new

---
+++ database.py
@@ -0,0 +1,41 @@
+import psycopg2
+#shimi shimi ai
+
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        database="student_db",
+        user="postgres",
+        password="YOUR_PASSWORD",
+        port="5432"
+    )
+
+def add_student(name, age, email):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO students (name, age, email)
+    VALUES (%s, %s, %s)
+    """
+
+    cursor.execute(query, (name, age, email))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    print("Student added successfully!")
+
+def view_students():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM students")
+
+    students = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return students
