f58c43ef2e47f9b90c3f988fb6da332d83484aed
Suleman <salmaan.waheed78910@gmail.com>
2026-07-21 06:18:42 +0500

    naksd

---
+++ test_connection.py
@@ -0,0 +1,13 @@
+from database import get_connection
+#database
+#it it
+#asiodlh
+try:
+    conn = get_connection()
+    print("✅ Connected to PostgreSQL successfully!")
+
+    conn.close()
+
+except Exception as e:
+    print("❌ Connection failed")
+    print(e)
